47f2716c42d4dd0dc992b49ffff8f2e601a9bb60
David Livingstone <david@david.tl>
2016-04-14 15:29:35 -0700

    CSS tweaks

--- scripts/appView.js
+++ scripts/appView.js
@@ -2,18 +2,12 @@
 
 var appView = {};
 
-// appView.shortenDescription = function() {
-//   var x = $('article p').text().substring(0,30);
-//   console.log(x);
-//   $('article p').text(x);
-// };
-
 appView.handleMainNav = function() {
   $('nav').on('click', '.tab-button', function(e) {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
-    $('nav .tab:first').click();
   });
+  $('nav .tab-button:first').click();
 };
 
 $(document).ready(function() {
