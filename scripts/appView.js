fb261ddb7a557fa068de67ac29c339ae6f7692db
David Livingstone <david@david.tl>
2016-04-15 13:59:44 -0700

    some filter functionality built

--- scripts/appView.js
+++ scripts/appView.js
@@ -16,7 +16,34 @@ appView.handleNavButton = function() {
   });
 };
 
+appView.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      var category = $(this).attr('data-category');
+      if ($('#category-filter option[value ="' + category + '"]').length === 0) {
+        $('#category-filter').append('<option value="' + category + '">' + category + '</option>');
+      }
+      var year = $(this).attr('data-year');
+      if (($('#year-filter option[value = "' + year + '"]').length === 0)) {
+        $('#year-filter').append('<option value="' + year + '">' + year + '</option>');
+      }
+    }
+  });
+};
+
+appView.handleResize = function() {
+  $(window).resize(function() {
+    if ($('#nav-button').css('display') === 'block') {
+      $('#main-nav').hide();
+    } else {
+      $('#main-nav').show();
+    }
+  });
+};
+
 $(document).ready(function() {
   appView.handleMainNav();
   appView.handleNavButton();
+  appView.populateFilters();
+  appView.handleResize();
 });
