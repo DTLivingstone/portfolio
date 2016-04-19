b6743d9a433e40936370b6a511b012afc84bdae7
David Livingstone <david@david.tl>
2016-04-19 09:00:29 -0700

    moved data to json

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,8 @@ function Project(opts) {
   // };
 };
 
+Project.all = [];
+
 Project.prototype.toHtml = function() {
   this.year = this.pubDate.slice(0,4);
   this.daysAgo = parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000);
@@ -21,14 +23,33 @@ Project.prototype.toHtml = function() {
   return template(this);
 };
 
-projectData.forEach(function(obj) {
-  projects.push(new Project(obj));
-});
+Project.loadAll = function(data) {
+  data.forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
 
-projects.sort(function(a,b) {
-  return (new Date(b.pubDate)) - (new Date(a.pubDate));
-});
+  data.sort(function(a,b) {
+    return (new Date(b.pubDate)) - (new Date(a.pubDate));
+  });
+};
 
-projects.forEach(function(a) {
-  $('#projects').append(a.toHtml());
-});
+Project.fetchAll = function() {
+  $.ajax({
+    type: 'HEAD',
+    url: 'assets/json/portfolio-data.json',
+    success: function(data, message, xhr){
+      var eTag = xhr.getResponseHeader('eTag');
+      if ((eTag) === localStorage.eTag) {
+        Project.loadAll(JSON.parse(localStorage.storedProjects));
+        appView.initIndexPage();
+      } else {
+        $.getJSON('assets/json/portfolio-data.json', function(data) {
+          Project.loadAll(data);
+          localStorage.storedProjects = JSON.stringify(data);
+          localStorage.eTag = eTag;
+          appView.initIndexPage();
+        });
+      }
+    }
+  });
+};
